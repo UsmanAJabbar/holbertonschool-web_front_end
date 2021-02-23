3d929c1aec8b5482bdf1322f89b050531a3d3a85
UsmanGTA <hi@usmanjabbar.com>
2021-02-24 01:45:42 +0500

    Added initial commit for task 1

--- 0x0E-Cookies_local_storage/src/index.js
+++ 0x0E-Cookies_local_storage/src/index.js
@@ -5,11 +5,17 @@
 setCookies = () => {
 	let firstname = document.getElementById("firstname").value;
 	let email = document.getElementById("email").value;
+	let expiry = new Date();					/* Create date object */
+	expiry.setDate(expiry.getDate() + 10);		/* SetDate + 10 days to expiry */
+	expiry = expiry.toUTCString();				/* Extract string and re-assign */
 
 	document.cookie = `firstname=${firstname}`;
 	document.cookie = `email=${email}`;
+	document.cookie = `Expires=${expiry}`;
 }
 showCookies = () => {
+
+	/* Clear the only paragraph tag if it exists */
 	if( document.querySelector('p') != null )
 		document.querySelector('p').remove();
 
