4faa460d9088613f93d6f6df5364a2ad20391670
UsmanGTA <hi@usmanjabbar.com>
2021-02-25 00:03:20 +0500

    Added initial commit for task 6

---
+++ 0x0E-Cookies_local_storage/src/sessionStorage.js
@@ -0,0 +1,33 @@
+let availableItems = ['Shampoo', 'Soap', 'Sponge', 'Water'];
+
+addItemToCart = (item) => {
+	sessionStorage.setItem(item, true);
+}
+createStore = () => {
+	let ul = document.createElement('ul');
+	ul.id = 'ls-list';
+
+	for (item of availableItems) {
+		let li = document.createElement('li');
+		li.innerText = item;
+		li.onclick = function() { addItemToCart(this.innerText) }
+		ul.append(li);
+	  }
+	document.body.append(ul);
+}
+displayCart = () => {
+	if (window.sessionStorage) {
+		let numItems = window.sessionStorage.length;
+		let p = document.createElement('p');
+
+		p.innerText = `You previously had ${numItems} items in your cart`;
+		document.body.append(p);
+	}
+	/* else pass */
+}
+if(typeof(sessionStorage) == undefined)
+	alert('Sorry, your browser does not support Web storage. Try again with a better one');
+else {
+	createStore();
+	displayCart();
+}
