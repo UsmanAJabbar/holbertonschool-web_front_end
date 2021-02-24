bdcfdb85cb318546fa212ac031650c9ac19a12fc
UsmanGTA <hi@usmanjabbar.com>
2021-02-24 23:55:53 +0500

    Added onclick fixes for on li items

--- 0x0E-Cookies_local_storage/src/localStorage.js
+++ 0x0E-Cookies_local_storage/src/localStorage.js
@@ -1,19 +1,19 @@
 let availableItems = ['Shampoo', 'Soap', 'Sponge', 'Water'];
 
 addItemToCart = (item) => {
-	window.localStorage.setItem(item, true);
+	localStorage.setItem(item, true);
 }
 createStore = () => {
 	let ul = document.createElement('ul');
 	ul.id = 'ls-list';
 
 	for (item of availableItems) {
+		console.log(item)
 		let li = document.createElement('li');
-		li.onclick = () => { addItemToCart(item) };
 		li.innerText = item;
-
+		li.onclick = function() { addItemToCart(this.innerText) }
 		ul.append(li);
-	}
+	  }
 	document.body.append(ul);
 }
 displayCart = () => {
