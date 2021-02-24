85dd546d06d2433c52398a0e90cae955c4bf4360
UsmanGTA <hi@usmanjabbar.com>
2021-02-24 07:13:46 +0500

    Added initial commit for task 2

--- 0x0E-Cookies_local_storage/src/index.js
+++ 0x0E-Cookies_local_storage/src/index.js
@@ -19,11 +19,25 @@ showCookies = () => {
 	if( document.querySelector('p') != null )
 		document.querySelector('p').remove();
 
-	let cookies = document.cookie.split(';')
+	let cookies = document.cookie.split('; ');
 	let p = document.createElement("p");
 
+	let cookieJar = {}
 	for (pears of cookies)
-		p.innerHTML += pears.split('=')[0] + ': ' + pears.split('=')[1];
+		cookieJar[ pears.split('=')[0] ] = pears.split('=')[1];
 
+	p.innerHTML = `Email: ${cookieJar['email']} - Firstname: ${cookieJar['firstname']}`;
 	document.body.append(p);
+}
+getCookie = (name) => {
+	let cookies = document.cookie.split('; ');
+
+	for (pear of cookies) {
+		let key = pear.split('=')[0];
+		if(key == name) {
+			let value = pear.split('=')[1];
+			return value;
+		}
+	}
+	return "";
 }
